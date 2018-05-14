034124d5ae1054c70ef7d5d4b8fecfaa5cbf7b73
Michael Rooney <mikeprooney@gmail.com>
2018-05-14 00:52:50 +0000

    Add CSV endpoint

--- geojson.php
+++ geojson.php
@@ -1,7 +1,8 @@
 <?php
 // kudos to http://stackoverflow.com/a/18106727/1778785 for snippet of PHP to read Google spreadsheet as CSV
 
-$googleSpreadsheetUrl = "https://docs.google.com/spreadsheet/pub?key=10eNXFh6mzFtii7B2PW90jmHtrQLJlRCrf3kkHU0HIH8&single=true&gid=0&output=csv";
+//$googleSpreadsheetUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSbgr20Ylk30r5d5-ujazRBHoS9Ky5DmYcuUmNGCfFN3-q2ZnvFZ6_UVU5H3LUXF8hH1duPglCvs6ZD/pub?output=csv";
+$googleSpreadsheetUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSbgr20Ylk30r5d5-ujazRBHoS9Ky5DmYcuUmNGCfFN3-q2ZnvFZ6_UVU5H3LUXF8hH1duPglCvs6ZD/pub?output=csv"
 
 $rowCount = 0;
 $features = array();
