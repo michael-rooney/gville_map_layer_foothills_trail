70cd0e0cffdacbf9fdb0ad717bdc1c969b3a1bd3
Michael Rooney <mikeprooney@gmail.com>
2018-05-14 01:17:39 +0000

    updated html file to remove notes & property 3

--- geojson.php
+++ geojson.php
@@ -37,8 +37,6 @@ if ( !$error && (($handle = fopen($googleSpreadsheetUrl, "r")) !== FALSE) )
       ),
       'properties' => array(
         'title' => $csvRow[2],
-        'notes' => $csvRow[3],
-        'property3' => $csvRow[4]
       )
     );
   } // end while, loop through CSV data
